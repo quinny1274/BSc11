4b258d8b18a5d657043c1f10d052d15f055c921a
unknown <evan.julian.quinn@gmail.com>
2024-05-14 16:50:52 +0100

    moved plants to new routes file

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var createRouter = require('./routes/create');
+var plantsRouter = require('./routes/plants');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/create', createRouter);
+app.use('/plants', plantsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

0f9799fbfb6e93c8cec84368f40a7483035947f0
AdenManson <adenmanson01@gmail.com>
2024-05-14 17:29:52 +0100

    Added filtering and styling changes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var plants = require('../controllers/plants');
 var suggestionsController = require('../controllers/suggestions');
+const {fetchDBpediaData} = require("../controllers/plants");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -45,77 +46,36 @@ router.get('/plants/:id', async function(req, res) {
     if (!plant) {
       return res.status(404).send('Plant not found');
     }
-    let nickname = "bob"; //When nickname is implemented all occurances of bob should be replaced with the actual nickname
-    let wasPlantCreatedByUser = plant.nickname === nickname;
+
+    const nickname = "bob"; // When nickname is implemented all occurrences of bob should be replaced with the actual nickname
+    const wasPlantCreatedByUser = plant.nickname === nickname;
     const suggestions = await getSuggestions(wasPlantCreatedByUser, plantId, nickname);
 
-    const plantName = plant.name;
-    const resource = `http://dbpedia.org/resource/${plantName}`;
-    const endpointUrl = 'https://dbpedia.org/sparql';
-    const sparqlQuery = `
-  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-  PREFIX dbo: <http://dbpedia.org/ontology/>
-  SELECT ?uri ?label ?comment
-  WHERE {
-    ?uri rdfs:label ?label .
-    ?uri rdfs:comment ?comment .
-    FILTER (langMatches(lang(?label), "en")) .
-    FILTER (langMatches(lang(?comment), "en")) .
-    FILTER (?uri = <${resource}>)
-  }
-`;
-
-    const encodedQuery = encodeURIComponent(sparqlQuery);
-    const url = `${endpointUrl}?query=${encodedQuery}&format=json`;
-
-    fetch(url)
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('DBpedia query failed');
-          }
-          return response.json();
-        })
-        .then(data => {
-          const bindings = data.results.bindings;
-          if (bindings.length > 0) {
-            res.render('plants', {
-              title: 'Plant Page',
-              suggestions: suggestions,
-              wasPlantCreatedByUser: wasPlantCreatedByUser,
-              plant: plant,
-              dbp_title: bindings[0].label.value,
-              dbp_comment: bindings[0].comment.value,
-              dbp_uri: bindings[0].uri.value
-            });
-          } else {
-            // Render the page without DBpedia stuff
-            res.render('plants', {
-              title: 'Plant Page',
-              suggestions: suggestions,
-              wasPlantCreatedByUser: wasPlantCreatedByUser,
-              plant: plant,
-              dbp_title: '', // Provide empty values for DBpedia properties
-              dbp_comment: '',
-              dbp_uri: ''
-            });
-          }
-        })
-        .catch(error => {
-          console.error('Error fetching data from DBpedia:', error);
-          // Render the page without DBpedia stuff
-          res.render('plants', {
-            title: 'Plant Page',
-            suggestions: suggestions,
-            wasPlantCreatedByUser: wasPlantCreatedByUser,
-            plant: plant,
-            dbp_title: '', // Provide empty values for DBpedia properties
-            dbp_comment: '',
-            dbp_uri: ''
-          });
-        });
-  } catch (err) {
-    console.error('Error fetching plant:', err);
-    res.status(500).send('Internal Server Error');
+    // Fetch DBpedia data
+    try {
+      const dbpediaData = await fetchDBpediaData(plant.name);
+      // Render page
+      res.render('plants', {
+        title: 'Plant Page',
+        suggestions: suggestions,
+        wasPlantCreatedByUser: wasPlantCreatedByUser,
+        plant: plant,
+        ...dbpediaData
+      });
+    } catch (dbpediaError) {
+      // Handle DBpedia fetch error
+      res.render('plants', {
+        title: 'Plant Page',
+        suggestions: suggestions,
+        wasPlantCreatedByUser: wasPlantCreatedByUser,
+        plant: plant,
+        ...dbpediaData
+      });
+    }
+  } catch (plantError) {
+    // Handle plant retrieval error
+    console.error('Error fetching plant:', plantError);
+    res.status(500).send('Error fetching plant');
   }
 });
 
@@ -132,15 +92,26 @@ async function getSuggestions(wasPlantCreatedByUser, plantId, nickname) {
   return suggestions;
 }
 
-// // Example usage:
-// const plantName = "Rose";
-// queryDBpedia(plantName)
-//     .then(data => {
-//         console.log(data);
-//         // Process data here
-//     })
-//     .catch(error => {
-//         console.error(error);
-//     });
+router.get('/explore/sort/date_time', function(req, res, next) {
+  let result = plants.getAll({date: -1})
+
+  console.log("Sort date time");
+
+  result.then(plants => {
+    let data = JSON.parse(plants);
+    res.render('explore', { title: 'Explore', data: data });
+  })
+});
+
+router.get('/explore/sort/identification', function(req, res, next) {
+  let result = plants.getAll({name: 1}) // TODO: add additional enableUserSuggestions filter
+
+  console.log("Sort date time");
+
+  result.then(plants => {
+    let data = JSON.parse(plants);
+    res.render('explore', { title: 'Explore', data: data });
+  })
+});
 
 module.exports = router;

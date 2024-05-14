02cf301e1391e15e96d84138cb918811935bb348
Xinkuu <kieransaw0721@gmail.com>
2024-05-15 00:03:38 +0100

    Added Username to be stored as cookie.
    Whenever a user fills in the text field and submit, the username will be stored as a cookie.

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
+// Route to handle form submission
+router.post('/submit', (req, res) => {
+  const {username} = req.body;
+  if (username) {
+    res.cookie('username', username)
+    res.redirect('explore');
+    var cookie = getCookie(req)
+    console.log(cookie)
+  }
+  else {
+    res.status(400).send('Username is required')
+  }
+
+});
+
+// Gets the Cookie for Username for testing.
+function getCookie(req) {
+  var cookie = req.headers.cookie;
+  return cookie.split('; ');
+}
 
 module.exports = router;
